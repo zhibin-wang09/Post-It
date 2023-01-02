38b13f1e69576b896719f9c6d3fc1fce6bddd627
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-01-02 15:24:16 -0500

    Add the delete functionality and modify the display method to innerHTML instead of using appendChild.

--- public/app.js
+++ public/app.js
@@ -7,36 +7,43 @@ async function showNotes(){
     try {
         const {data} = await axios.get('http://localhost:5500/note')
         const notes = data.notes
-    notes.forEach(writtenNote => {
-        const note = document.createElement('div')
-        const header = document.createElement('div')
-        header.setAttribute('class','header')
-        //Creat title
-        const title = document.createElement('h3')
-        title.setAttribute('class','noteTitle')
-        title.innerHTML = writtenNote.title
-        //Create delete button
-        const deleteBtn = document.createElement('button')
-        deleteBtn.setAttribute('class','deleteBtn')
-        deleteBtn.innerHTML = "Delete Note"
-        //Create edit button
-        const editBtn = document.createElement('button')
-        editBtn.setAttribute('class','editBtn')
-        editBtn.innerHTML = "Edit Note"
-        //Insert to header
-        header.appendChild(title)
-        header.appendChild(deleteBtn)
-        header.appendChild(editBtn)
-        //Create note content
-        const content = document.createElement('p')
-        content.innerHTML = writtenNote.note
+        //Using note.forEach() is not as good as notes.map() because 
+            let acc = "";
+        notes.forEach(writtenNote => {
+            const note = document.createElement('div')
+            const header = document.createElement('div')
+            header.setAttribute('class','header')
+            //Creat title
+            const title = document.createElement('h3')
+            title.setAttribute('class','noteTitle')
+            title.innerHTML = writtenNote.title
+            //Create delete button
+            const deleteBtn = document.createElement('button')
+            deleteBtn.setAttribute('class','delete-btn')
+            deleteBtn.setAttribute('data-id', writtenNote._id)
+            deleteBtn.innerHTML = "Delete Note"
+            //Create edit button
+            const editBtn = document.createElement('button')
+            editBtn.setAttribute('class','edit-btn')
+            editBtn.setAttribute('data-id' , writtenNote._id)
+            editBtn.innerHTML = "Edit Note"
+            //Insert to header
+            header.appendChild(title)
+            header.appendChild(deleteBtn)
+            header.appendChild(editBtn)
+            //Create note content
+            const content = document.createElement('p')
+            content.innerHTML = writtenNote.note
 
-        note.appendChild(header)
-        note.appendChild(content)
-        note.setAttribute('class', 'note-history-display')
-        noteDisplay.appendChild(note)
-    });
+            note.appendChild(header)
+            note.appendChild(content)
+            note.setAttribute('class', 'note-history-display')
+            acc += note.outerHTML;
+        });
+        noteDisplay.innerHTML = acc;
+
     } catch (error) {
+        noteDisplay.innerHTML = "<h3>There was a error...</h3>"
         console.log(error)
     }
 }
@@ -50,7 +57,24 @@ form.addEventListener('submit', async (e) => {
             title: title.value,
             note: noteContent.value
         })
+        noteContent.value =''
+        title.value = ''
         showNotes()
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+noteDisplay.addEventListener('click' , async (e) => {
+    e.preventDefault()
+    try {
+        const id = e.target.dataset.id
+        if(e.target.className === "delete-btn"){
+            await axios.delete(`http://localhost:5500/note/${id}`)
+            const header = e.target.parentElement.parentElement
+            header.remove()
+            showNotes()
+        }
     } catch (error) {
         console.log(error)
     }
