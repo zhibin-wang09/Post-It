b628573be430c3e1cd016925bec6e12c4076c8b5
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-01-21 12:56:06 -0500

    Refactor one file into multiple files

--- client/src/note.js
+++ client/src/note.js
@@ -85,3 +85,5 @@ class Note extends React.Component{
       }
     }
   }
+
+export default Note
---
+++ client/src/notecontainer.js
@@ -0,0 +1,104 @@
+/*global chrome*/
+import React from 'react'
+import Note from './note.js'
+var url = ""
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+        url = tabs[0].url;
+        const urlobj = new URL(url)
+        url = urlobj.origin
+        console.log(url)
+});
+class NoteContainer extends React.Component{
+    constructor(props){
+      super(props)
+      this.state = {
+        notes: [],
+        title : '',
+        note : '',
+        webaddress: url
+      }
+      this.componentDidMount = this.componentDidMount.bind(this)
+    }
+  
+    setTitle = (e)=> {
+      this.setState({title:e.target.value})
+    }
+  
+    setNote = (e) => {
+      this.setState({note:e.target.value})
+    }
+  
+    handleSubmit = async (e) => {
+      e.preventDefault()
+      const note = {
+        title: this.state.title,
+        note: this.state.note,
+        webaddress: url
+      }
+      try {
+        await fetch('https://post-it-api.onrender.com/note' , {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(note)
+        })
+        console.log("POST SUCCESS")
+        this.setState({
+          title:'',
+          note: ''
+        })
+        this.componentDidMount()
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    async componentDidMount() {
+      try {
+        const res = await fetch('https://post-it-api.onrender.com/note',{
+              method:"GET",
+        })
+        const data = await res.json()
+        const filtered = data.notes.filter((res) => {
+          if(res.webaddress === url){
+            return res
+          }
+        })
+        this.setState({
+          notes : filtered
+        })
+        console.log("GET SUCCESS")
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    render(){
+      const noteFetch = this.state.notes.map((note) => {
+        return(
+          <div className='note-history-display' key={note._id}>
+            <form>
+              <Note _id={note._id} title={note.title} content={note.note} refetch={this.componentDidMount}/>
+            </form>
+          </div>
+        )
+      })
+      return (
+        <>
+          <div id="note-taking-area">
+            <form id="noteInfo" onSubmit={this.handleSubmit}>
+                <input id="title" placeholder="name of the note" type="text" name="title" value={this.state.title} onChange ={this.setTitle}/>
+                <textarea id="text-area" name="note" placeholder="notes..." value={this.state.note} onChange={this.setNote}></textarea>
+                <button type="submit" className="save-btn">Save</button>
+            </form>
+          </div>  
+          <div className='note-history-display-container'>
+            {noteFetch}
+          </div>
+        </>
+      )
+    }
+}
+
+export default NoteContainer
