c1b76b006bc7d702bdfade697a7f8c086a0e6106
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-01-21 12:46:33 -0500

    Revert "Merge pull request #4 from zhibin-wang09/liftUpReact"
    This reverts commit 738a5ffbb0d22033e2753754998772644cd92c78, reversing
    changes made to 4954adfc8727c58345c5a415b32dfc3507290466.

--- client/src/index.js
+++ client/src/index.js
@@ -4,26 +4,147 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Note from './note.js'
 var url = ""
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         url = tabs[0].url;
         const urlobj = new URL(url)
         url = urlobj.origin
-        console.log(url)
 });
+
+//Function component takes props in parameter
+class Note extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      title : this.props.title,
+      note : this.props.note,
+      editMode : false
+    }
+  }
+
+  handleDelete = async(e) => {
+    e.preventDefault()
+    await fetch(`https://localhost/note/${this.props._id}`, {
+      method: "DELETE"
+    })
+    await this.props.refetch()
+  }
+
+  handleEdit = (e) =>{
+    e.preventDefault()
+    this.setState({editMode : true, title: this.props.title, note: this.props.content})
+  }
+
+  handleSave = async (e) => {
+    e.preventDefault()
+    await fetch(`https://localhost/note/${this.props._id}`, {
+      method: "PATCH",
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          title: this.state.title,
+          note: this.state.note
+      })
+    })
+    await this.props.refetch()
+    this.setState({editMode: false})
+  }
+
+  setTitle = (e)=> {
+    this.setState({title:e.target.value})
+  }
+
+  setNote = (e) => {
+    this.setState({note:e.target.value})
+  }
+
+
+  render(){
+    if(!this.state.editMode){
+      return (
+        <>
+          <div className='header'>
+            <h3 className='note-title'>{this.props.title}</h3>
+            <button className='delete-btn' type='button' data-id={this.props._id} onClick={this.handleDelete}>Delete Note</button>
+            <button className='edit-btn' type='button' data-id={this.props._id} onClick={this.handleEdit}>Edit Note</button>
+          </div>
+          <p>{this.props.content}</p>
+        </>
+        )
+    }else{
+      return (
+        <>
+          <div className='header'>
+            <input className='update-title' value={this.state.title} onChange={this.setTitle}/>
+            <button className='delete-btn' type='button' data-id={this.props._id} onClick={this.handleDelete}>Delete Note</button>
+            <button className='edit-btn' type='button' data-id={this.props._id} onClick={this.handleEdit}>Edit Note</button>
+          </div>
+          <textarea className='edit-text-area' value={this.state.note} onChange={this.setNote}></textarea>
+          <button className='save-btn' onClick={this.handleSave}>Update</button>
+        </>
+        )
+    }
+  }
+}
 
 
 class NoteContainer extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      notes: [],
-      title : '',
-      note : '',
-      webaddress: url
+      notes: []
     }
     this.fetchData = this.fetchData.bind(this)
   }
 
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://post-it-api.onrender.com/note',{
+            method:"GET",
+      })
+      const data = await res.json()
+      const filtered = data.notes.filter((res) => {
+        if(res.webaddress === url){
+          return res
+        }
+      })
+      this.setState({
+        notes : filtered
+      })
+      console.log("GET SUCCESS")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render(){
+    const noteFetch = this.state.notes.map((note) => {
+      return(
+        <div className='note-history-display' key={note._id}>
+          <form>
+            <Note _id={note._id} title={note.title} content={note.note} refetch={this.fetchData}/>
+          </form>
+        </div>
+      )
+    })
+    return (
+      <div className='note-history-display-container'>
+        {noteFetch}
+      </div>
+    )
+  }
+}
+
+/* class Form extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      title : '',
+      note : '',
+      webaddress: url
+    }
+  }
+      
   setTitle = (e)=> {
     this.setState({title:e.target.value})
   }
@@ -48,75 +169,29 @@ class NoteContainer extends React.Component{
         body: JSON.stringify(note)
       })
       console.log("POST SUCCESS")
-      this.setState({
-        title:'',
-        note: ''
-      })
-      this.componentDidMount()
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async fetchData(){
-    try {
-      const res = await fetch('https://post-it-api.onrender.com/note',{
-        method: 'GET',
-      })
-      const data = await res.json()
-      const filtered = data.notes.filter((res) => {
-        if(res.webaddress === url){
-          return res
-        }
-      })
-      this.setState({
-        notes: filtered
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async componentDidMount() {
-    try {
-      this.fetchData()
-      console.log("GET SUCCESS")
     } catch (error) {
       console.log(error)
     }
   }
 
   render(){
-    const noteFetch = this.state.notes.map((note) => {
-      return(
-        <div className='note-history-display' key={note._id}>
-          <form>
-            <Note _id={note._id} title={note.title} content={note.note} refetch={this.fetchData}/>
-          </form>
-        </div>
-      )
-    })
     return (
-      <>
-        <div id="note-taking-area">
+      <div id="note-taking-area">
           <form id="noteInfo" onSubmit={this.handleSubmit}>
               <input id="title" placeholder="name of the note" type="text" name="title" value={this.state.title} onChange ={this.setTitle}/>
               <textarea id="text-area" name="note" placeholder="notes..." value={this.state.note} onChange={this.setNote}></textarea>
               <button type="submit" className="save-btn">Save</button>
           </form>
-        </div>  
-        <div className='note-history-display-container'>
-          {noteFetch}
-        </div>
-      </>
+      </div>  
     )
   }
-}
+} */
 
 class Base extends React.Component{
   render(){
     return(
       <>
+      <Form/>
       <NoteContainer/>
       </>
     )
