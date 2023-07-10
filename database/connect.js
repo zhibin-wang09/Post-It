0827ee2d918590536ae790ce3a07c8194f405666
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-10 13:43:19 -0400

    Added additional object ----- • Instead of only the connection function, now the class return a db object where it contains the connection object but also other relevant data such as the user model and role model

--- database/connect.js
+++ database/connect.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 
 mongoose.set('strictQuery', 'true') //strict query
 
+
 const connection =  async (uri) => {
     await mongoose.connect(uri);
-
 }
 
-module.exports = connection
+const db = {}
+db.connect = connection
+db.user = require("../model/user.model.js")
+db.role = require("../model/role.model.js")
+db.ROLES = ["user", "admin" , "moderator"]
+
+module.exports = db
