30a814a5c7189968c92cef37de89be7c62117a57
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-11 11:18:11 -0400

    Add identifier to ntoe

--- model/note.model.js
+++ model/note.model.js
@@ -6,6 +6,10 @@ const mongoose = require('mongoose')
 
 
 const noteSchema = new mongoose.Schema({
+    userId: {
+        type:String,
+        required: true
+    },
     title : {
         type: String,
         required: true
