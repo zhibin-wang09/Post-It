9c8750cb82b4c8e13c3a4f3842117dcc8fc397c8
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-10 22:51:35 -0400

    Included middleware for the endpoints ----- • Added authorization middleware before user can access the notes • Added more endpoints for the signup, login, signout actions

--- router/route.js
+++ router/route.js
@@ -5,12 +5,20 @@
  */
 const express = require('express')
 const router = express.Router()
+const middleware = require('../middleware/middlewares.js')
 const {getAllNote,
     postNote,
     deleteNote,
-    updateNote} = require('../controller/cntrl.js')
-router.route('/note').get(getAllNote).post(postNote)
+    updateNote} = require('../controller/note.cntrl.js')
+const {signup, signin, signout} = require("../controller/auth.cntrl.js") // controllers for signin functions
+const auth = middleware.authJwt.verifyToken
+const checkSignup = middleware.verifySignUp.checkDuplicateUsernameOrEmail
+router.route('/note').get(auth,getAllNote).post(auth,postNote)
 //router.route('/api/v1/notes') //Create a new note //Get all
-router.route('/note/:id').delete(deleteNote).patch(updateNote) //Access individual notes with delete and update functionality
+router.route('/note/:id').delete(auth,deleteNote).patch(auth,updateNote) //Access individual notes with delete and update functionality
+
+router.route('/signup').post(checkSignup,signup)
+router.route('/signin').post(signin) 
+router.route('/signout').post(signout)
 
 module.exports = router
