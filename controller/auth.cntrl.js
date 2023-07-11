6672c457ec2ea841ae52fa7cbb79deefa7bc02be
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-10 22:49:46 -0400

    Created Controllers ----- • Created a authorization controller where it will used to handle requests such as login, signup, signout. • Changed the name of the note controller file to make it easier to understand

---
+++ controller/auth.cntrl.js
@@ -0,0 +1,66 @@
+/**
+ * This controller manages all the sign in, sign up, sign out requests from the user 
+ */
+
+const user = require("../model/user.model.js")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const config = require("../config/auth.config.js")
+
+signup = async (req,res) => {
+    try{
+        const {username, email, password} = req.body
+        const hash = await bcrypt.hash(password,10)
+        await user.create({
+            username: username,
+            email : email,
+            password: hash, 
+        })
+        res.status(200).json({message: "User created successfully!"})
+    }catch(err){
+        res.status(400).send({message: err})
+    }
+}
+
+signin = async (req,res) => {
+    const {username,password, role} = req.body
+
+    const userInDB = await user.findOne({username: username})
+    if(!userInDB){
+        res.status(400).send({message: "User not found!"})
+        return
+    }
+    // check password
+    const isPasswordValid = bcrypt.compareSync(password, userInDB.password)
+    if(!isPasswordValid){
+        res.status(400).send({message: "Invalid password!"})
+        return
+    }
+
+    // create a jwt token
+    const token = jwt.sign({id: userInDB.id}, 
+                            config.secret,
+                            {
+                                algorithm: 'HS256',
+                                allowInsecureKeySizes: true,
+                                expiresIn: 86400*30
+                            })
+    
+    // res.cookie("access-token", token, {
+    //     maxAge: 86400*30
+    // })
+    req.session.token = token;
+    
+    res.status(200).send({message: "Logged in"})
+}
+
+signout = async(req, res) => {
+    try{
+        req.session = null
+        return res.status(200).send({message: "You have been signed out!"})
+    }catch(err){
+        this.next(err);
+    }
+}
+
+module.exports = {signup, signin, signout}
---
+++ controller/note.cntrl.js
@@ -0,0 +1,61 @@
+/**
+ * This is the controller of the application, where it contains the 
+ * logic of the application and manipulates or updates the model. Base
+ * on the user input received from the view.
+ * 
+ * 
+ */
+
+
+const note = require('../model/note.model.js')
+
+
+const getAllNote = async (req,res)=> {
+    try {
+        const data = await note.find({})
+        res.status(200).json({msg:"Get success", notes: data})
+    } catch (error) {
+        res.status(400).json({msg:"Failed",error:error})
+    }
+}
+
+const postNote = async (req,res) => {
+   try {
+        const title = req.body.title
+        const noteContent = req.body.note
+        const createdNote = await note.create({
+            title:title, 
+            note:noteContent,
+            webaddress: req.body.webaddress
+        })
+        res.status(200).json({msg:"Success", _id:createdNote._id})
+   } catch (error) {
+        res.status(400).json({msg:"Failed",error:error})
+   }
+}
+
+const deleteNote = async (req,res) => {
+    try {
+        await note.deleteOne({_id:req.params.id})
+        res.status(200).json({msg:"Success"})
+    } catch (error) {
+        res.status(400).json({msg:"Failed",error:error})
+    }
+}
+
+const updateNote = async (req,res) => {
+    try {
+        await note.updateOne({_id:req.params.id},{
+            title:req.body.title,
+            note:req.body.note})
+        res.status(200).json({msg:"Patch success"})
+    } catch (error) {
+        res.status(400).json({msg:"Failed",error:error})
+    }
+    
+}
+
+module.exports = {getAllNote,
+                    postNote,
+                    deleteNote,
+                    updateNote}
