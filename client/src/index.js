4916bba0b3080c5c56b859a8d6e0475f992111d0
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-12 15:11:47 -0400

    Created routes for the children pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import NoteContainer from './notecontainer'
 
+import './index.css'
+import './note.css'
+import './auth.css'
+import NoteContainer from './note/notecontainer'
+import Login from './auth/login'
+import App from './app'
+import Home from './home'
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        children : [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path:'/login',
+                element: <Login/>
+            },
+            {
+                path: '/note',
+                element: <NoteContainer/>
+            },
+        ]
+        
+    },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<NoteContainer/>)
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+)
