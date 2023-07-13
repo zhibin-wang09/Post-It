88ca11f7f9eb703fbaf8805f3a6434193778bdde
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-13 15:32:04 -0400

    Use memoryrouter instead to accomdate chrome extension

--- client/src/app.js
+++ client/src/app.js
@@ -1,19 +1,24 @@
-import {Link, Outlet} from 'react-router-dom'
+import {Link,Routes,Route} from 'react-router-dom'
 import React from 'react'
+import Login from './auth/login'
+import NoteContainer from './note/notecontainer'
+import Home from './home'
 
 function App(){
     return (
         <>
             <nav id="nav-bar">
                 <ul id="nav-bar-items">
-                    <li id><Link to='/'>Home</Link></li>
+                    <li><Link to='/'>Home</Link></li>
                     <li><Link to='/login'>Login</Link></li>
                     <li><Link to='/note'>Notes</Link></li>
                 </ul>
             </nav>
-            <div>
-                <Outlet/>
-            </div>
+            <Routes>
+                <Route path='/' element= {<Home/>}/>
+                <Route path='/login' element= {<Login/>}/>
+                <Route path='/note' element ={<NoteContainer/>}/>
+            </Routes>
         </>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,38 +4,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './note.css'
 import './auth.css'
-import NoteContainer from './note/notecontainer'
-import Login from './auth/login'
 import App from './app'
-import Home from './home'
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App/>,
-        children : [
-            {
-                index: true,
-                element: <Home/>
-            },
-            {
-                path:'/login',
-                element: <Login/>
-            },
-            {
-                path: '/note',
-                element: <NoteContainer/>
-            },
-        ]
-        
-    },
-])
+import {MemoryRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <MemoryRouter>
+            <App/>
+        </MemoryRouter>
     </React.StrictMode>
 )
